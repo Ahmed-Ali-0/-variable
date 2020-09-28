922261fab41039cbda75326b19cd00bf269e9a32
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-09-28 13:30:30 -0400

    add a text

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    word_Press_Buttun_A = 0
+})
 // Created By: Ahmed Ali
 // 
 // Created on: Sep 28, 2020.
 // 
 // This program use  variable.
 let word_Press_Buttun_A = 0
+basic.showString("Created by Ahmed Ali")
