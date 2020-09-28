ca1f48643578aa7c83a251e913ccbb392d3f3fb0
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-09-28 14:39:23 -0400

    add comment

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    word_Press_Buttun_A += 1
+    number_press_buutn_A += 1
+    basic.showString("" + (number_press_buutn_A))
 })
 input.onButtonPressed(Button.B, function () {
-    word_Press_Buttun_A += -4
+    number_press_buutn_A += -1
+    basic.showString("" + (number_press_buutn_A))
 })
 // Created By: Ahmed Ali
-// 
 // Created on: Sep 28, 2020.
 // 
 // This program use  variable.
-let word_Press_Buttun_A = 0
+let number_press_buutn_A = 0
+number_press_buutn_A = 0
